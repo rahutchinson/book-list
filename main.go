c16d1c9d74a4b1a82e346833e899ddc066021286
Ryan Hutchinson <ryanahutchinson@gmail.com>
2022-03-07 22:01:09 -0600

    added post to it

--- main.go
+++ main.go
@@ -15,8 +15,8 @@ import (
 
 var (
 	httpAddr = flag.String("http", defaultAddr(), "http listen address")
-
-	index *template.Template
+	postKey  = os.Getenv("POST_KEY")
+	index    *template.Template
 )
 
 type books struct {
@@ -24,16 +24,25 @@ type books struct {
 }
 
 type book struct {
+	ISBN        string `json:"isbn"`
 	Name        string `json:"name"`
+	Author      string `json:"author"`
 	Type        string `json:"type"`
 	Description string `json:"description"`
 	Cover       string `json:"cover"`
+	Genre       string `json:"genre"`
+	Tags        string `json:"tags"`
 	Link        string `json:"link"`
 }
 
 type indexParams struct {
 	Books books
 	Host  string
+}
+
+type postBook struct {
+	Book book
+	Key  string
 }
 
 func main() {
@@ -49,26 +58,73 @@ func main() {
 }
 
 func indexHandler(w http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/" {
-		http.NotFound(w, req)
-		return
+	switch req.Method {
+	case http.MethodGet:
+		params := indexParams{
+			Books: readBooks(),
+			Host:  req.Host,
+		}
+		w.Header().Set("Cache-Control", "no-cache")
+		index.Execute(w, params)
+	case http.MethodPost:
+	case http.MethodPut:
+		// Update an existing record.
+	case http.MethodDelete:
+		// Remove the record.
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
-
-	params := indexParams{
-		Books: readBooks(),
-		Host:  req.Host,
-	}
-	w.Header().Set("Cache-Control", "no-cache")
-	index.Execute(w, params)
 }
 
 func bookHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	s := readBooks()
-	err := json.NewEncoder(w).Encode(s)
-	if err != nil {
-		return
+	switch req.Method {
+	case http.MethodGet:
+	case http.MethodPost:
+		var b postBook
+		err := json.NewDecoder(req.Body).Decode(&b)
+		if err != nil {
+			http.Error(w, "Bad POST", 400)
+		}
+		if b.Key == postKey {
+			repeat := addToBook(b.Book)
+			if !repeat {
+				http.Error(w, "repeat or bad book", http.StatusConflict)
+			}
+		} else {
+			http.Error(w, "F off", 404)
+		}
+
+	case http.MethodPut:
+		// Update an existing record.
+	case http.MethodDelete:
+		// Remove the record.
+	default:
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func addToBook(b book) bool {
+	if b.ISBN == "" || b.Link == "" || b.Name == "" {
+		return false
+	}
+	exsistingBooks := readBooks().Books
+
+	var repeat bool
+	for _, book := range exsistingBooks {
+		repeat = b.ISBN == book.ISBN
+	}
+	if repeat {
+		return false
+	}
+	booksToWrite := append(exsistingBooks, b)
+	writeBooks(books{Books: booksToWrite})
+	fmt.Println("write to file")
+	return true
+}
+
+func writeBooks(toWrite books) {
+	file, _ := json.MarshalIndent(toWrite, "", " ")
+	_ = ioutil.WriteFile("books.json", file, 0644)
 }
 
 func readBooks() books {
